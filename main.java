5eb0273bbb38c212a49324d971fb9b696de24cf8
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-12-03 19:18:58 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,32 @@
+public class Checkerboard {
+
+    public static void main(String[] args) {
+
+        int length = 16;
+        int height = 8;
+
+        for (int i = 0; i <= height; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.out.print("* ");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
